527b03e0e28c9d5e73fca7297a9043059c8ddf9a
ia3andy <adamevin@redhat.com>
2018-09-13 12:10:06 +0200

    build: make KC env vars

--- launcher-creator-frontend/src/creator/service/auth/KeycloakConfig.ts
+++ launcher-creator-frontend/src/creator/service/auth/KeycloakConfig.ts
@@ -1,6 +1,6 @@
 export class KeycloakConfig {
   public enabled = true;
-  public clientId = 'openshiftio-public';
-  public realm = 'rh-developers-launch';
-  public url= 'https://sso.openshift.io/auth';
+  public clientId = process.env.REACT_APP_KEYCLOAK_CLIENT_ID;
+  public realm = process.env.REACT_APP_KEYCLOAK_REALM;
+  public url= process.env.REACT_APP_KEYCLOAK_URL;
 }
